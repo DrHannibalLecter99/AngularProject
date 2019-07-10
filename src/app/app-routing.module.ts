d0405a588f48984b36aee9d4463d33a49430d795
manish sharma <1801manish1112@gmail.com>
2019-07-10 20:02:53 +0530

    new files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"Home",pathMatch:"full"}
+  ,{path:"Home",loadChildren: () => import('./home/home.module').then(m=>m.HomeModule)}
+  ,{path:"Products", loadChildren:()=> import('./products/products.module').then(m=>m.ProductsModule)}
+  ,{path:"AboutUs",loadChildren:() => import('./about-us/about-us.module').then(m=>m.AboutUsModule)}
+  ,{path:"ContactUs",loadChildren:() => import('./contact-us/contact-us.module').then(m=>m.ContactUsModule)}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    console.log('App Module');
+  }
+}
